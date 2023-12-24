3099f06e9e194acedf754995c36b8ed1b85543b8
jp <jpaguinsky@gmail.com>
2023-12-24 00:59:15 -0300

    Add test
    adds test dir and support for Junit to later develop some tests

---
+++ test/java/com/craftinginterpreters/lox/LoxTest.java
@@ -0,0 +1,16 @@
+package com.craftinginterpreters.lox.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LoxTest {
+
+    @Test
+    public void testSomeFunctionality() {
+        // Your test code here
+        assertTrue(true); // Replace with actual test logic
+    }
+
+    // Add more test methods as needed
+}
+ 
