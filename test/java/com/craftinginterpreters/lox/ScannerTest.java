d59979eb68f88719b259d9e7b20be2e41e805b4d
jp <jpaguinsky@gmail.com>
2023-12-31 13:20:29 -0300

    Test ScannerTest
    Created template class with tests that are to be implemented.

---
+++ test/java/com/craftinginterpreters/lox/ScannerTest.java
@@ -0,0 +1,49 @@
+package com.craftinginterpreters.lox;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ScannerTest {
+
+    @Test
+    public void testSingleLineCommentHandling() {
+        // Test code for handling single-line comments
+   }
+
+    @Test
+    public void testMultiLineCommentHandling() {
+        // Test code for handling multi-line comments
+    }
+
+    @Test
+    public void testKeywordTokenization() {
+        // Test code for tokenizing keywords (e.g., if, while)
+    }
+
+    @Test
+    public void testBooleanLiteralTokenization() {
+        // Test code for tokenizing boolean literals (true, false)
+    }
+
+    @Test
+    public void testStringLiteralTokenization() {
+        // Test code for tokenizing string literals
+    }
+
+    @Test
+    public void testSpecialCharacterTokenization() {
+        // Test code for tokenizing special characters (e.g., +, -, *, /)
+    }
+
+    @Test
+    public void testCombinedScenarios() {
+        // Test code with a combination of different tokens and scenarios
+    }
+
+    @Test
+    public void testComplexInput() {
+        // Test code with a more complex input string
+    }
+
+    // Add more test methods as needed
+
+}
