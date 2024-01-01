df2934e204ee291b8956bf395c99a7e84a4b0458
jp <jpaguinsky@gmail.com>
2024-01-01 15:25:39 -0300

    Add ScannerTest.java
    Adds testing for single line comments

--- test/java/com/craftinginterpreters/lox/ScannerTest.java
+++ test/java/com/craftinginterpreters/lox/ScannerTest.java
@@ -1,12 +1,27 @@
 package com.craftinginterpreters.lox;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.List;
+import java.util.ArrayList;
+import static org.junit.Assert.assertArrayEquals;
+
 
 public class ScannerTest {
 
     @Test
     public void testSingleLineCommentHandling() {
         // Test code for handling single-line comments
+	Scanner scanner = new Scanner("var // test comment int");
+	List<Token> scannedList = new ArrayList<>(scanner.scanTokens());
+	List<Token> tokenList =  new ArrayList<>();
+	tokenList.add(new Token(TokenType.VAR, "var", null, 1 ));
+	tokenList.add(new Token(TokenType.EOF, "", null, 2 ));
+	System.out.println(scannedList);
+	System.out.println(tokenList);
+	System.out.println("Scanned List Class: " + scannedList.getClass());
+	System.out.println("Token List Class: " + tokenList.getClass());
+	System.out.println("Lists Are Equal: " + scannedList.equals(tokenList));
+	assertEquals(scannedList, tokenList);	 
    }
 
     @Test
