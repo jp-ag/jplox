277120be7998b346074bd8f9abf9a822de945728
jp <jpaguinsky@gmail.com>
2024-01-01 15:12:46 -0300

    Update Token.java
    Fixes typos and adds more information on its toString method. Also adds equalsmethod for comparison in tokens.

--- src/main/java/com/craftinginterpreters/lox/Token.java
+++ src/main/java/com/craftinginterpreters/lox/Token.java
@@ -1,19 +1,31 @@
 package com.craftinginterpreters.lox;
+import java.util.Objects;
 
 class Token {
     final TokenType type;
-    final String lexemme;
+    final String lexeme;
     final Object literal;
     final int line;
 
-    Token(TokenType type, String lexemme, Object literal, int line){
+    Token(TokenType type, String lexeme, Object literal, int line){
         this.type = type;
-        this.lexemme = lexemme;
+        this.lexeme = lexeme;
         this.literal = literal;
         this.line = line;
     }
+    //currently not comparing lines, because they are used for error reporting    
+    @Override
+    public boolean equals(Object obj){
+	if (this == obj){return true;}
+	if (obj == null || getClass() != obj.getClass()){return false;}
+	Token token = (Token) obj;
+	return type == token.type &&
+	       lexeme.equals(token.lexeme)  &&
+	       Objects.equals(literal, token.literal);
+    }
 
     public String toString(){
-        return type + " " + lexemme + " " + literal;
+        if(literal == null){return "Type: " + type + " Lexeme: " + lexeme + " Literal and literal class: " + literal + " null";}
+	return "Type: " + type + " Lexeme: " + lexeme + " Literal and literal class" + literal + " " + literal.getClass();
     }
 }
